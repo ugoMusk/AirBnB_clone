77225b11faeaf17700b1619d3781f9d74397b5a4
SAMKE-glitch <sammythemwa@gmail.com>
2023-07-15 03:15:26 -0700

    edited the file

--- models/base_model.py
+++ models/base_model.py
@@ -9,40 +9,45 @@ from datetime import datetime
 class BaseModel:
     """
     Representing a class
-    Defines attributes/methods for subclasses to inherit
     """
 
     def __init__(self):
-        """ initialiaze class instance """
+        """
+        Initializing the BaseModel class
+        """
 
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        """ print human readable output """
-        
+        """
+        str representation
+        """
+
         return f"{[self.__class__.__name__]} {(self.id)} {self.__dict__}"
 
     def save(self):
-        """ save updates of class instance attributes """
+        """
+        a method that updates the public instance
+        attribute updated_at with current datetime
+        """
 
         updated_at = datetime.now()
 
     def to_dict(self):
-        """ dictionary representation of our classe instance attributes 
-        returns a dictionary containing all keys/value of __dict__
-        of the instance:
-            self.__dict__, a key __class__ must be added to the dictionary
-            created_at, updated_at must be converted to string object
-            in ISO format"""
+        """
+        Returns a dictionary representation of keys and values
+        of self.__dict__, a key __class__ must be added to the
+        dictionary created_at, updated_at must be converted to
+        string object in ISO format
+        """
 
         to_dictFormat = {}
-        # adding a class key to identify class name
+        # Adding a class key to identify class name
         # of the instance attribute
 
         to_dictFormat["__class__"] = self.__class__.__name__
-
         for key, val in self.__dict__.items():
             if isinstance(val, datetime):
                 to_dictFormat[key] = val.isoformat()
