9859db1fd67e20cdb0a72c53a699b9886aa36491
SAMKE-glitch <sammythemwa@gmail.com>
2023-07-15 02:45:19 -0700

    added some changes on dict

--- models/base_model.py
+++ models/base_model.py
@@ -1,44 +1,54 @@
-#!/usr/bin/python3
+#!/usr/bin/pythoni3
 """
 A module that defines a class called BaseModel
 """
-
-
-import uuid
+from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
     """
+    Representing a class
     Defines attributes/methods for subclasses to inherit
     """
 
     def __init__(self):
         """ initialiaze class instance """
-        self.id = str(uuid.uuid4())
+
+        self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
         """ print human readable output """
-        return "[{}] {} {}".format(type(self).__name__,
-                                   self.id, type(self).__dict__)
+        
+        return f"{[self.__class__.__name__]} {(self.id)} {self.__dict__}"
 
     def save(self):
         """ save updates of class instance attributes """
-        self.created_at = datetime.now()
-        return self.created_at
+
+        updated_at = datetime.now()
 
     def to_dict(self):
-        """ dictionary representation of our classe instance attributes """
+        """ dictionary representation of our classe instance attributes 
+        returns a dictionary containing all keys/value of __dict__
+        of the instance:
+            self.__dict__, a key __class__ must be added to the dictionary
+            created_at, updated_at must be converted to string object
+            in ISO format"""
 
-        self.created_at = self.created_at.isoformat()
-        self.updated_at = self.updated_at.isoformat()
-        return {"__class__": getattr(self, "__class__"),
-                "id": getattr(self, "id"), "created_at":
-                getattr(self, "created_at"), "updated_at":
-                getattr(self, "updated_at")}
+        to_dictFormat = {}
+        # adding a class key to identify class name
+        # of the instance attribute
+
+        to_dictFormat["__class__"] = self.__class__.__name__
+
+        for key, val in self.__dict__.items():
+            if isinstance(val, datetime):
+                to_dictFormat[key] = val.isoformat()
+            else:
+                to_dictFormat[key] = val
+        return to_dictFormat
 
 
-i_obj = BaseModel()
-print(i_obj)
+
