a6b41560ae7ba4731a5578fbbef3295eeacda1b8
ugoMusk <ugobothankgod@gmail.com>
2023-07-16 17:55:07 +0100

    create state, city and amenity classes

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module that defines a class called City
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    class representing a City
+    """
+    state_id = ""
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module that defines a class called State
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    class representing a State
+    """
+    name = ""
