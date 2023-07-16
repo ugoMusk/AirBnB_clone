776ba090cf3a3accb9797ed8ed659cf90b64a11c
SAMKE-glitch <sammythemwa@gmail.com>
2023-07-16 03:37:29 -0700

    updated console

--- console.py
+++ console.py
@@ -32,6 +32,27 @@ class HBNBCommand(cmd.Cmd):
             b.save()
             print(b.id)
 
+    def do_show(self, line):
+        """ Prints string representation of an instance """
+        if line = "" or line is None:
+            print("** class name missing **")
+        else:
+            words = line.split(" ")
+            if words[0] not in storage.classes():
+                print("** class doesn't exist **")
+            elif len(words) < 2:
+                print("** instance id missing **")
+            else:
+                key = "{}.{}".format(words[0], words[1])
+                if key not in storage.all():
+                    print("** no instance found **")
+                else:
+                    print(storage.all()[key])
+
+    def do_destroy(self, line):
+        """ Deletes an instance based on the name and the id"""
+
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
