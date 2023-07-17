639b7b1d348c0ad22fa6a461132acd0fea7ff29c
ugoMusk <ugobothankgod@gmail.com>
2023-07-17 13:04:34 +0100

    write unittests for BaseModel

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,17 +1,100 @@
 #!/usr/bin/python3
-""" Write unittests for BaseModel """
+""" Module that tests all BaseModel methods and attributes """
 
-import datetime
-import json
 import os
-from models.base_model import BaseModel
+import json
 import unittest
+import datetime
+from models.base_model import BaseModel
 from uuid import UUID
 
 
 class test_basemodel(unittest.TestCase):
-    """ Test class for BaseModel """
-    pass
+    """ class that tests methods of BaseModel """
+
+    def __init__(self, *args, **kwargs):
+        """ Constructor for test_basemodel class """
+        super().__init__(*args, **kwargs)
+        self.name = 'BaseModel'
+        self.value = BaseModel()
+
+    def setUp(self):
+        """ """
+        pass
+
+    def tearDown(self):
+        """ """
+        try:
+            os.remove('file.json')
+        except Exception:
+            pass
+
+    def test_default(self):
+        """ """
+        obj = self.value()
+        self.assertEqual(type(obj), self.value)
+
+    def test_kwargs(self):
+        """ """
+        obj = self.value()
+        copy = obj.to_dict()
+        with self.assertFalse(new is obj):
+            new = BaseModel(**copy)
+
+    def test_kwargs_int(self):
+        """ """
+        obj = self.value()
+        copy = obj.to_dict()
+        copy.update({1: 2})
+        with self.assertRaises(TypeError):
+            new = BaseModel(**copy)
+
+    def test_save(self):
+        """ test save method """
+        obj = self.value()
+        obj.save()
+        key = self.name + "." + obj.id
+        with open('file.json', 'r') as file:
+            json_format = json.load(file)
+            self.assertEqual(json_format[key], obj.to_dict())
+
+    def test_str(self):
+        """ test string method """
+        obj = self.value()
+        self.assertEqual(str(obj), f'{self.name} {obj.id} {obj.__dict__}')
+
+    def test_todict(self):
+        """ test for to_dict method """
+        obj = self.value()
+        dict_obj = obj.to_dict()
+        self.assertEqual(obj.to_dict(), dict_obj)
+
+    def test_kwargs_none(self):
+        """ test against correct arguments type"""
+        arg= {None: None}
+        with self.assertRaises(TypeError):
+            new = self.value(**arg)
+
+    def test_kwargs_one(self):
+        """ test against correct key """
+        arg = {'Name': 'test'}
+        with self.assertRaises(KeyError):
+            new = self.value(**arg)
+
+    def test_id(self):
+        """ test id's attribute type """
+        new = self.value()
+        self.assertEqual(type(new.id), str)
+
+    def test_created_at(self):
+        """ test created_at attribute type """
+        new = self.value()
+        self.assertEqual(type(new.created_at), datetime.datetime)
+
+    def test_updated_at(self):
+        """ test updated_at attribute type"""
+        new = self.value()
+        self.assertEqual(type(new.updated_at), datetime.datetime)
 
 
 if __name__ == "__main__":
