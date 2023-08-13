d1f6bd4373385317a6dff343fe2eafc1ccd63c65
ugoMusk <ugobothankgod@gmail.com>
2023-08-13 09:14:17 +0100

    reformat

--- models/base_model.py
+++ models/base_model.py
@@ -24,14 +24,13 @@ class BaseModel:
         if kwargs is not None and kwargs != {}:
             for key in kwargs:
                 if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                            kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__["created_at"] = datetime.strptime(kwargs["created_at"],
+                                                                    "%Y-%m-%dT%H:%M:%S.%f")
                 elif key == "updated_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                            kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__["created_at"] = datetime.strptime(kwargs["updated_at"],
+                                                                    "%Y-%m-%dT%H:%M:%S.%f")
                 else:
                     self.__dict__[key] = kwargs[key]
-        
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
